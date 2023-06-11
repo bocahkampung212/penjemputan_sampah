a48a9404d02adc5ce38869466c08c5c81ae80ae1
ryowex16 <118143935+bocahkampung212@users.noreply.github.com>
2023-06-11 22:07:21 +0700

    App penjemputan sampah

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:penjemputan_sampah/views/login_view.dart';
+import 'package:penjemputan_sampah/views/registrasi.dart';
+
+void main(List<String> args) {
+  runApp(const MaterialApp(
+    home: LoginView(),
+    
+  ));
+}
---
+++ lib/views/registrasi.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class registrasi extends StatelessWidget {
+  const registrasi({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(42, 235, 109, 1),
+      body: Stack(
+        children: [],
+      ),
+    );
+  }
+}
